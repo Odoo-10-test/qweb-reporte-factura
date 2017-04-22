5841fba7d5bbb5ace56bd88d1a89ff7637df0fe7
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2017-04-22 14:43:49 -0300

    1
    1

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'MFH Reporte de Factura',
+    'author': 'Marlon Falcón Hernández',
+    'category': 'Sales',
+    'description': """
+Ejemplo de módulo
+=====================================================
+        """,
+    'version': '10.0.0.1',
+    'data': [
+              'views/account_report_new.xml',
+              'views/mfh_report_invoice.xml',
+    ],
+    'demo': [],
+    'auto_install': False,
+}
